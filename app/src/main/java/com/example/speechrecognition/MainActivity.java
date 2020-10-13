5634306eee1714a9be446ba18275d227ad768468
mailongd <longm4020@gmail.com>
2020-10-13 17:12:53 +1100

    MASTERED VERSION EXE

--- app/src/main/java/com/example/speechrecognition/MainActivity.java
+++ app/src/main/java/com/example/speechrecognition/MainActivity.java
@@ -1,35 +1,19 @@
 package com.example.speechrecognition;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.icu.text.StringSearch;
-import android.media.tv.TvContract;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.speech.RecognizerIntent;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.w3c.dom.Text;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,27 +31,12 @@ public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextTv = (TextView) findViewById(R.id.textTv);
-        mTextResponses = (TextView) findViewById(R.id.textTv);
         mVoiceBtn = (ImageButton) findViewById(R.id.voiceBtn);
-
-        //create array list and connect it to xml file
-        wordBank = Arrays.asList(getResources().getStringArray(R.array.Words));
-        responses = Arrays.asList(getResources().getStringArray(R.array.responses));
-
-        //button click to show speech to text dialog
-        mVoiceBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                speak();
-            }
-        });
 
         bottomNavigationView = findViewById(R.id.bottom_nav);
 
@@ -97,47 +66,6 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             };
-
-
-    public void speak() {
-        //intent to show speech to text dialog
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi say something");
-
-        //start intent
-        try {
-            //in there was no error
-            //show dialog
-            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
-        } catch (Exception e) {
-            //if there was some error
-            //get message of error and show
-            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-
-        //this is the counter for the responses loop
-        int responseItem =0;
-        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK) {
-            ArrayList<String> result = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            mTextTv.setText(result.get(0).toUpperCase());
-            //loops every word in a phrase until it keyword from wordBank
-            for(String wordItem: wordBank) {
-                if (result.get(0).contains(wordItem)) {
-                    mTextResponses.setText(responses.get(responseItem).toString());
-                    break;
-                }
-                //item +1
-                //this moves to the next word, and it will check if there is key word in word item
-                responseItem++;
-            }
-        }
-    }
 }
+
+
--- app/src/main/java/com/example/speechrecognition/MicFragment.java
+++ app/src/main/java/com/example/speechrecognition/MicFragment.java
@@ -1,64 +1,90 @@
 package com.example.speechrecognition;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MicFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
+
+
 public class MicFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MicFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MicFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MicFragment newInstance(String param1, String param2) {
-        MicFragment fragment = new MicFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
+    //views from activity
+    TextView mTextTv;
+    TextView mTextResponses;
+    ImageButton mVoiceBtn;
+    List<String> wordBank = new ArrayList<>();
+    List<String> responses = new ArrayList<>();
+    boolean word = false;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mic, container, false);
     }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTextTv = (TextView) getView().findViewById(R.id.textTv);
+        mTextResponses = (TextView) getView().findViewById(R.id.textTv);
+        mVoiceBtn = (ImageButton) getView().findViewById(R.id.voiceBtn);
+        wordBank = Arrays.asList(getResources().getStringArray(R.array.Words));
+        responses = Arrays.asList(getResources().getStringArray(R.array.responses));
+
+        //button click to show speech to text dialog
+        mVoiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+    }
+
+    public void speak() {
+        //intent to show speech to text dialog
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi say something");
+        startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        //this is the counter for the responses loop
+        int responseItem = 0;
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK) {
+            ArrayList<String> result = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            mTextTv.setText(result.get(0).toUpperCase());
+            //loops every word in a phrase until it keyword from wordBank
+            for (String wordItem : wordBank) {
+                if (result.get(0).contains(wordItem)) {
+                    mTextResponses.setText(responses.get(responseItem).toString());
+                    break;
+                }
+                //item +1
+                //this moves to the next word, and it will check if there is key word in word item
+                responseItem++;
+            }
+        }
+    }
 }
+
